403a74ea869ccbb8a034cee21665bc3756f7af87
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2022-10-10 14:34:59 +0100

    added all phone images

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 import { phones } from "/phones.js";
 
-console.log(phones);
+const diplayPhones = document.querySelector("#displayPhones");
+
+for (let i = 0; i < Object.keys(phones).length; i++) {
+  let phone = document.createElement("div");
+  phone.innerHTML = `
+    <div class="card">
+    <image src="" class="cardImage">
+    <h3>${phones[i].name}</h3>
+    <p>${phones[i].brand}</p>
+    <p>${phones[i].memory}</p>
+    <p>${phones[i].cost}</p>
+    <p>${phones[i].colors}</p>
+    </div>
+    `;
+  displayPhones.appendChild(phone);
+}

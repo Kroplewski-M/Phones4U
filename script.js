ced038afcbbfd883be1fe2ebea091d6ccbf4ccfd
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2022-10-17 12:43:02 +0100

    implementing filter

--- script.js
+++ script.js
@@ -18,23 +18,41 @@ maxSlider.oninput = function () {
 
 //GET REST OF FILTERS
 let brand = document.querySelector("#selectBrand");
-let color = document.querySelector("colors");
-let memory = document.querySelector("memoryBtn");
+let color = document.querySelector("#colors");
+let memory = document.querySelector("#memoryBtn");
+brand.value = "Apple";
+color.value = "black";
+//FILTER PHONE OBJECT
+const filterPhones = Object.entries(phones);
+const filtered = filterPhones.filter(function ([key, value]) {
+  return (
+    value.cost > minSlider.value &&
+    value.cost < maxSlider.value &&
+    (memory.value != "any" ? value.memory == memory.value : value.memory) &&
+    (brand.value != "any" ? value.brand == brand.value : value.brand) &&
+    (color.value != "any" ? value.colors.includes(color.value) : value.colors)
+  );
+});
+console.log(filtered);
+
 //DISPLAY PHONES
 const diplayPhones = document.querySelector("#displayPhones");
 
 for (let i = 0; i < Object.keys(phones).length; i++) {
+  var cols = phones[i].colors.join(", ");
   let phone = document.createElement("div");
   phone.classList.add("card");
+
   phone.innerHTML = `
-    <div class="cardImgCon">
-      <image src="./images/phones/${phones[i].ext}" class="cardImage">
-    </div>
-    <h3>${phones[i].name}</h3>
-    <p><span class="cardDesc">Brand:</span>${phones[i].brand}</p>
-    <p><span class="cardDesc">Memory:</span>${phones[i].memory}</p>
-    <p><span class="cardDesc">Price:</span> £${phones[i].cost}</p>
-    <p class="cardDesc">Colors:<span class="colors">${phones[i].colors}</span></p>
-    `;
+      <div class="cardImgCon">
+        <image src="./images/phones/${phones[i].ext}" class="cardImage">
+      </div>
+      <h3>${phones[i].name}</h3>
+      <p><span class="cardDesc">Brand:</span>${phones[i].brand}</p>
+      <p><span class="cardDesc">Memory:</span>${phones[i].memory}</p>
+      <p><span class="cardDesc">Price:</span> £${phones[i].cost}</p>
+      <p class="cardDesc">Colors:<span class="colors">${cols}</span></p>
+      `;
+
   displayPhones.appendChild(phone);
 }

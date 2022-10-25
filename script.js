9c8814cf3c06d22b564b76f8a1202f3ae83c79c8
Mateusz <kroplewskimateusz@gmail.com>
2022-10-25 02:27:59 +0100

    started working on filter system

--- script.js
+++ script.js
@@ -1,30 +1,12 @@
 import { phones } from "/phones.js";
 
-const diplayPhones = document.querySelector("#displayPhones");
-
-for (let i = 0; i < Object.keys(phones).length; i++) {
-  var cols = phones[i].colors.join(", ");
-  let phone = document.createElement("div");
-  phone.classList.add("card");
-  phone.innerHTML = `
-    <div class="cardImgCon">
-      <image src="./images/phones/${phones[i].ext}" class="cardImage">
-    </div>
-    <h3>${phones[i].name}</h3>
-    <p><span class="cardDesc">Brand:</span>${phones[i].brand}</p>
-    <p><span class="cardDesc">Memory:</span>${phones[i].memory}</p>
-    <p><span class="cardDesc">Price:</span> £${phones[i].cost}</p>
-    <p class="cardDesc">Colors:<span class="colors">${cols}</span></p>
-    `;
-  displayPhones.appendChild(phone);
-}
-
 const mobileMenuBtn = document.querySelector("#mobileMenuBtn");
 const mobileNav = document.querySelector("#mobileNav");
 const closeMobileNav = document.querySelector("#closeMobile");
 const filterButton = document.querySelector("#filterButton");
 const filter = document.querySelector("#filter");
 
+//SHOW MOBILE MENUS
 let showMobileNav = false;
 let showMobileFilter = false;
 function toggleMobileNav() {
@@ -55,4 +37,58 @@ closeMobileNav.addEventListener("click", () => {
 filterButton.addEventListener("click", () => {
   showMobileFilter = !showMobileFilter;
   toggleMobileFilter();
+});
+
+//UPDATE SLIDER
+let minPrice = document.querySelector("#minPrice");
+let maxPrice = document.querySelector("#maxPrice");
+let minPriceValue = document.querySelector("#minPriceValue");
+let maxPriceValue = document.querySelector("#maxPriceValue");
+
+minPrice.addEventListener("input", () => {
+  minPriceValue.innerHTML = "£" + minPrice.value;
+});
+maxPrice.addEventListener("input", () => {
+  maxPriceValue.innerHTML = "£" + maxPrice.value;
+});
+
+const diplayPhones = document.querySelector("#displayPhones");
+
+for (let i = 0; i < Object.keys(phones).length; i++) {
+  var cols = phones[i].colors.join(", ");
+  let phone = document.createElement("div");
+  phone.classList.add("card");
+  phone.innerHTML = `
+    <div class="cardImgCon">
+      <image src="./images/phones/${phones[i].ext}" class="cardImage">
+    </div>
+    <h3>${phones[i].name}</h3>
+    <p><span class="cardDesc">Brand:</span>${phones[i].brand}</p>
+    <p><span class="cardDesc">Memory:</span>${phones[i].memory}</p>
+    <p><span class="cardDesc">Price:</span> £${phones[i].cost}</p>
+    <p class="cardDesc">Colors:<span class="colors">${cols}</span></p>
+    `;
+  displayPhones.appendChild(phone);
+}
+
+//FILTER
+let filteredItems = [];
+const filterPhones = Object.entries(phones);
+
+function filterAllPhones() {
+  filteredItems = [];
+  let inputs = document.getElementsByTagName("input");
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].type == "checkbox") {
+      if (inputs[i].checked == true) {
+        filteredItems.push(inputs[i].value.toString());
+      }
+    }
+  }
+  console.log(filteredItems);
+}
+
+let searchBtn = document.querySelector("#searchBtn");
+searchBtn.addEventListener("click", () => {
+  filterAllPhones();
 });
